84a5a9d066cf66c6c9221bbbfc979bd9cc8768d1
Warnakulaisuru <warnakulaisuru@gmail.com>
2025-06-17 02:52:06 +0530

    update package img part

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 import tourRoute from './routes/tours.js';
 import userRoute from './routes/users.js';
 import authRoute from './routes/auth.js';
@@ -20,6 +23,10 @@ dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Required to use `__dirname` in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const corsOption = {
   origin: 'http://localhost:3000',
@@ -39,6 +46,8 @@ const connect = async()=>{
     }
 }
 
+// Serve static files
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 //middleware
 app.use(express.json())
 app.use(cors(corsOption))
@@ -48,6 +57,8 @@ app.use('/api/v1/tours',tourRoute);
 app.use('/api/v1/users',userRoute);
 app.use('/api/v1/review',reviewRoute);
 app.use('/api/v1/booking',bookingRoute);
+
+
 
 app.listen(port, ()=>{
     connect()
